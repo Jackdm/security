48f81624c0c93e5d6f052d6d661fcaefe9f9d13a
Jackdm <jacckdm@gmail.com>
2017-05-20 11:08:20 -0700

    Added some things -Jack

--- SecurityApp/AmIPwned.swift
+++ SecurityApp/AmIPwned.swift
@@ -8,3 +8,21 @@
 
 import Foundation
 
+import UIKit
+
+var unfData : String!
+let url = URL(string: "https://haveibeenpwned.com/api/v2/breachedaccount/george@gmail.com?truncateResponse=true")!
+
+let task = URLSession.shared.dataTask(with: url) { data, response, error in
+    guard let data = data, error == nil else {
+        print("\(String(describing: error))")
+        return
+    }
+    
+    unfData = String(data: data, encoding: .utf8)
+    
+    print(unfData)
+    
+    
+}
+
