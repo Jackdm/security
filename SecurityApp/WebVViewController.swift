66b5859908b4409f961831c4ef72409812ab509a
Megan Kaye <megan.kaye38@gmail.com>
2017-05-21 02:22:27 -0400

    added view controller

---
+++ SecurityApp/WebVViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebVViewController.swift
+//  SecurityApp
+//
+//  Created by Megan Kaye on 5/21/17.
+//  Copyright © 2017 jackdm. All rights reserved.
+//
+
+import UIKit
+
+class WebVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
